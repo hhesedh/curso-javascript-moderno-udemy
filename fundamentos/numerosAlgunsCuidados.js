7cb84d086af165ef996aba31648f7b871c17c00b
fabio <fabiodeoliveira07@gmail.com>
2018-01-20 13:51:55 -0200

    Adicao de alguns exemplos importantes

--- fundamentos/numerosAlgunsCuidados.js
+++ fundamentos/numerosAlgunsCuidados.js
@@ -1,5 +1,20 @@
 console.log(7 / 0)
+/*
+ * Cuidados com operacoes
+ * entre strings e number
+ * no caso da soma, ele 
+ * entende como concatenacao
+ * no caso de outros operadores
+ * aritmeticos, ele efetua uma 
+ * operacao algebrica.
+ */
 console.log("10" / 2)
+console.log("10" + 2)
+console.log("10" - 2)
+
+// Not a Number
 console.log("Show!" * 2)
+
+// Imprecisao de ponto flutuante
 console.log(0.1 + 0.7)
 // console.log(10.toString())
