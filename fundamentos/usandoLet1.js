72c8dab408a677d099221955cf3bb4c5cb050782
fabio <fabiodeoliveira07@gmail.com>
2018-01-21 01:50:13 -0200

    Exemplos de definicao de espoco entre var e let

---
+++ fundamentos/usandoLet1.js
@@ -0,0 +1,14 @@
+let numero = 1
+{
+    let numero = 2
+    console.log('Dentro =', numero)
+
+}
+console.log('Fora =', numero)
+
+/*
+ * variaveis definidas com a palavra reservada var
+ * tem escopo global e escopo de função 
+ * variaveis definidas com a palavra reservada let
+ * tem escopo globabl, escopo de função e escopo de bloco
+*/
