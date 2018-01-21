f9e7046cbf89cbb7007b018de33b44e788570c7a
fabio <fabiodeoliveira07@gmail.com>
2018-01-21 02:39:26 -0200

    Aprendendo como funciona o icamento em javascript

---
+++ fundamentos/hoisting.js
@@ -0,0 +1,9 @@
+/* Hoisting igual a içamento
+ * é como se a variavel fosse
+ * declarada no começo
+ * isso não funciona com let 
+ * nem com const
+*/  
+console.log('a =', a)
+var a = 2
+console.log('a =', a)
